01ffbf3c6f0c4c9ce383083f149a74250ea9840a
HeyDreaw <asuppinimusic@gmail.com>
2017-10-20 23:21:26 -0700

    resolving credentials issue

--- slothbot.rb
+++ slothbot.rb
@@ -2,16 +2,18 @@
 
 require 'twitter'
 
+def client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["consumer_key"]
+      config.consumer_secret = ENV["consumer_secret"]
+      config.access_token = ENV["access_token"]
+      config.access_token_secret = ENV["access_token_secret"]
+		end
+end
+
 client.search("#badday").take(10).each do |tweet|
   puts "#{tweet.user.screen_name}: #{tweet.text}"
   client.favorite(tweet)
 	client.update_with_media("@#{tweet.user.screen_name} Here's a picture of a sloth. I hope you have a better day!", File.new("cutesloth.jpg"),
 		in_reply_to_status_id: tweet.id)
-end
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['consumer_key']
-  config.consumer_secret = ENV['consumer_secret']
-  config.access_token = ENV['access_token']
-  config.access_token_secret = ENV['access_token_secret']
 end
